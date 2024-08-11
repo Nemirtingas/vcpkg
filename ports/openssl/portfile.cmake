80884288eb862599080f619c7684fbddc46e70c7
Nemirtingas <nemirtingas@noreply.com>
2024-08-11 20:00:45 +0200

    Fix openssl cross-compilation

--- ports/openssl/portfile.cmake
+++ ports/openssl/portfile.cmake
@@ -3,83 +3,43 @@ if(EXISTS "${CURRENT_INSTALLED_DIR}/share/libressl/copyright"
     message(FATAL_ERROR "Can't build openssl if libressl/boringssl is installed. Please remove libressl/boringssl, and try install openssl again if you need it.")
 endif()
 
-if(VCPKG_TARGET_IS_EMSCRIPTEN)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
+    REPO Nemirtingas/openssl-externalCMake
+    REF "${VERSION}"
+    SHA512 1ad9ef844426d5687bbe7e939aea5dc153a82d0fc65e6e2572fc8fdf73de00b3d363fa54c6329bfde1d47d961c63f6f32ba534bf7e7a0b5884c148af2cbc33e2
+)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH OPENSSL_SOURCE_PATH
     REPO openssl/openssl
     REF "openssl-${VERSION}"
     SHA512 5282e078eb205ef691647e07d65186d81f9328329a06cca209a138adf10b95f12c16742ac61b4ab86e5098c153645ea413088d8263ff7054fdd40decd85b271f
-    PATCHES
-        cmake-config.patch
-        command-line-length.patch
-        script-prefix.patch
-        asm-armcap.patch
-        windows/install-layout.patch
-        windows/install-pdbs.patch
-        unix/android-cc.patch
-        unix/move-openssldir.patch
-        unix/no-empty-dirs.patch
-        unix/no-static-libs-for-shared.patch
 )
 
-vcpkg_list(SET CONFIGURE_OPTIONS
-    enable-static-engine
-    enable-capieng
-    no-tests
-    no-docs
+file(COPY "${OPENSSL_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/openssl")
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    INVERTED_FEATURES
+        no-asm OPENSSL_ASM
 )
 
-set(INSTALL_FIPS "")
-if("fips" IN_LIST FEATURES)
-    vcpkg_list(APPEND INSTALL_FIPS install_fips)
-    vcpkg_list(APPEND CONFIGURE_OPTIONS enable-fips)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    vcpkg_list(APPEND CONFIGURE_OPTIONS shared)
-else()
-    vcpkg_list(APPEND CONFIGURE_OPTIONS no-shared no-module)
-endif()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/OpenSSL)
+vcpkg_fixup_pkgconfig()
 
-if(NOT "tools" IN_LIST FEATURES)
-    vcpkg_list(APPEND CONFIGURE_OPTIONS no-apps)
-endif()
-
-if("weak-ssl-ciphers" IN_LIST FEATURES)
-    vcpkg_list(APPEND CONFIGURE_OPTIONS enable-weak-ssl-ciphers)
-endif()
-
-if("ssl3" IN_LIST FEATURES)
-    vcpkg_list(APPEND CONFIGURE_OPTIONS enable-ssl3)
-    vcpkg_list(APPEND CONFIGURE_OPTIONS enable-ssl3-method)
-endif()
-
-if(DEFINED OPENSSL_USE_NOPINSHARED)
-    vcpkg_list(APPEND CONFIGURE_OPTIONS no-pinshared)
-endif()
-
-if(OPENSSL_NO_AUTOLOAD_CONFIG)
-    vcpkg_list(APPEND CONFIGURE_OPTIONS no-autoload-config)
-endif()
-
-if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
-    include("${CMAKE_CURRENT_LIST_DIR}/windows/portfile.cmake")
-    include("${CMAKE_CURRENT_LIST_DIR}/install-pc-files.cmake")
-else()
-    include("${CMAKE_CURRENT_LIST_DIR}/unix/portfile.cmake")
-endif()
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
-if (NOT "${VERSION}" MATCHES [[^([0-9]+)\.([0-9]+)\.([0-9]+)$]])
-    message(FATAL_ERROR "Version regex did not match.")
-endif()
-set(OPENSSL_VERSION_MAJOR "${CMAKE_MATCH_1}")
-set(OPENSSL_VERSION_MINOR "${CMAKE_MATCH_2}")
-set(OPENSSL_VERSION_FIX "${CMAKE_MATCH_3}")
-configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
-
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+#file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
